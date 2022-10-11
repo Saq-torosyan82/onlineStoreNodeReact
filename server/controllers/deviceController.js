9b751bae6f7b5ed07898d38834d3b88554bbf2d7
Saqo <saqo.torosyan82@gmail.com>
2022-10-11 19:01:34 +0400

    created controllers

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,15 @@
+class DeviceController {
+    async getAll(req, res) {
+
+    }
+
+    async getById(req, res) {
+
+    }
+
+    async create(req, res) {
+
+    }
+}
+
+module.exports = new DeviceController()
