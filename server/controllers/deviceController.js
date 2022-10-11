64a8715025f55b0d5b14b7c31569c9011cec6a72
Saqo <saqo.torosyan82@gmail.com>
2022-10-11 19:45:17 +0400

    implemented some controller methods

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,3 +1,8 @@
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError')
+const uuid = require('uuid')
+const path = require('path')
+
 class DeviceController {
     async getAll(req, res) {
 
@@ -7,8 +12,19 @@ class DeviceController {
 
     }
 
-    async create(req, res) {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
 
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const brand = await Device.create({name, price, brandId, typeId, img: fileName})
+            return res.json(brand)
+        } catch (e) {
+            return next(ApiError.badRequest(e.message))
+        }
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const sequelize = require('./db')
 const models = require('./models/models')
 const router = require('./routes/index')
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 app.use(errorHandler)
