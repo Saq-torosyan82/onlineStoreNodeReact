2962172240daca877ff82944ac1cfac3c576a287
Saqo <saqo.torosyan82@gmail.com>
2022-10-14 14:09:51 +0400

    client - created stores

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
 import App from './App';
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+export const Context = createContext(null)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <Context.Provider value={{
+        user: new UserStore(),
+        devices: new DeviceStore()
+    }}>
+        <App />
+    </Context.Provider>
 );
