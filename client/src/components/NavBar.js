0fb6addf1b2746d195e22c49bea641904a25650d
Saqo <saqo.torosyan82@gmail.com>
2022-10-14 14:45:19 +0400

    client - created authorization page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -40,7 +40,7 @@ const NavBar = observer(() => {
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Login</Button>
+                        <Button variant={"outline-light"} onClick={() => navigate(LOGIN_ROUTE)}>Login</Button>
                     </Nav>
                 }
             </Container>
