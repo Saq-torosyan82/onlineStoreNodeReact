0db81add769b5c7044a0146dddef48934e461f6b
Saqo <saqo.torosyan82@gmail.com>
2022-10-14 18:13:34 +0400

    client - implemented add type api

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,16 +1,15 @@
 import React, {useState} from 'react';
 import {Modal, Button, Form} from "react-bootstrap";
+import {createType} from "../../http/deviceApi";
 
 const CreateType = ({show, onHide}) => {
     const [value, setValue] = useState('')
 
     const addType = () => {
-        setValue('')
-        onHide()
-        /*createType({name: value}).then(data => {
+        createType({name: value}).then(data => {
             setValue('')
             onHide()
-        })*/
+        })
     }
 
     return (
